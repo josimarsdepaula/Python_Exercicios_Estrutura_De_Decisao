3f73448ac545c007e5a5d484fc8a6a9377ae67d5
Josimar <josimarsdepaula@gmail.com>
2024-07-29 18:53:42 -0300

    Alterações

---
+++ 20.py
@@ -0,0 +1,21 @@
+nota1 = float(input("informe a primeira nota: "))
+nota2 = float(input("informe a segunda nota: "))
+nota3 = float(input("informe a terceira nota: "))
+
+if not ( 0 <= nota1 <= 10 and 0 <= nota2 <= 10 and 0 <=  nota3 <= 10):
+    print("informe uma nota válida")
+
+else:
+    media = (nota1 + nota2 + nota3) / 3
+
+    if media == 10:
+        print("Aprovado com Distinção")
+
+    elif media >= 7:
+        print("Aprovado")
+    
+    else:
+        print("Reprovado")
+        
+    
+
